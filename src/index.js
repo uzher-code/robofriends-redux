5cb1b98bce8e80f7f82c68601eecd52abf33feee
uzher-code <uazhar.git@gmail.com>
2020-11-15 18:48:01 -0500

    Adding redux to manage state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 import App from './Containers/App';
+import { searchRobots, requestRobots } from './reducers';
+
+const logger = createLogger();
+
+const rootReducer = combineReducers({searchRobots, requestRobots})
+const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
 
 ReactDOM.render(
-	<App />
+	<Provider store = { store } >
+		<App />
+	</Provider>
 	,document.getElementById('root')
 );
 
