0db9e583845008d380ffbebc0bed449a7e9597e2
uzher-code <uazhar.git@gmail.com>
2021-01-17 15:06:53 -0500

    Improved accessibility for screen readers by wrapping form elements in labels

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import searchIcon from './images/searchIcon.png';
+import './SearchBox.css'
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	console.log("SearchBox")
 	return (
 		<div className ='pa2'>
-			<input
-				className = 'pa3 ba b--green bg-lightest-blue'
-				type='search' 
-				placeholder = 'search robots'
-				onChange = {searchChange}
-			/>
+			<label> 
+				<img className = 'box' alt="Search" src={searchIcon}/>
+				<input
+					className = 'pa3 ba b--green bg-lightest-blue'
+					type='search' 
+					placeholder = 'search robots'
+					onChange = {searchChange}
+				/>
+			</label>
 		</div>
 	)
 }
