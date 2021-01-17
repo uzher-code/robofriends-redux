30999e4f5e9b4f2b05449ba45b8f775212d4e790
uzher-code <uazhar.git@gmail.com>
2021-01-17 17:11:47 -0500

    Removed search icon and replaced the label tag with aria-label attribute

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -1,20 +1,16 @@
 import React from 'react';
-import searchIcon from './images/searchIcon.png';
-import './SearchBox.css'
 
 const SearchBox = ({ searchfield, searchChange }) => {
 	console.log("SearchBox")
 	return (
 		<div className ='pa2'>
-			<label> 
-				<img className = 'box' alt="Search" src={searchIcon}/>
-				<input
-					className = 'pa3 ba b--green bg-lightest-blue'
-					type='search' 
-					placeholder = 'search robots'
-					onChange = {searchChange}
-				/>
-			</label>
+			<input
+				aria-label = 'Search'
+				className = 'pa3 ba b--green bg-lightest-blue'
+				type='search' 
+				placeholder = 'search robots'
+				onChange = {searchChange}
+			/>
 		</div>
 	)
 }
